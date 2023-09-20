19b4e861558405e3c8731342b2217f1ac10b1879
SY97P <onetuks@naver.com>
2023-09-21 00:57:24 +0900

    feat: 유저 주문 조회 기능 추가

--- src/main/java/com/tangerine/theatre_manager/order/repository/OrderRepository.java
+++ src/main/java/com/tangerine/theatre_manager/order/repository/OrderRepository.java
@@ -1,8 +1,12 @@
 package com.tangerine.theatre_manager.order.repository;
 
 import com.tangerine.theatre_manager.order.model.Order;
+import com.tangerine.theatre_manager.user.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    Page<Order> findByUser(User user, Pageable pageable);
 }
